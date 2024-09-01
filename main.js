00d15c94cdf8554af838b4d2bcd277ddf278b50d
zoe-shih <zoe76102469@gmail.com>
2024-09-01 12:46:57 +0800

    初始化

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 import './assets/scss/all.scss';
+import 'bootstrap';
+
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
 
 console.log("Hello world!");
+
+
+
